2279300ab26faeb7347e49d967e25ad52be8eefd
rishabhdevsharma24 <rishabhdevsharma9@gmail.com>
2021-03-22 13:46:41 +0530

    added login and signup activities. including login_form.xml and signup_form.xml registered activities in manifest file

---
+++ app/src/main/java/com/adverse/foodorderingapp/activities/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.adverse.foodorderingapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.adverse.foodorderingapp.R;
+
+public class LoginActivity extends AppCompatActivity {
+    EditText etMobile,etpass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+}
