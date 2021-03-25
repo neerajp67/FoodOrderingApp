e7e3ae51316eedfa9258e6f91e3b8c48d519e3d7
neerajp67 <neerajpandey025@gmail.com>
2021-03-25 12:32:07 +0530

    added post api request functionality. files added: Api (Interface), RetrofitClient

--- app/src/main/java/com/adverse/foodorderingapp/activities/LoginActivity.java
+++ app/src/main/java/com/adverse/foodorderingapp/activities/LoginActivity.java
@@ -5,24 +5,65 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.adverse.foodorderingapp.R;
+import com.adverse.foodorderingapp.api.RetrofitClient;
+import com.adverse.foodorderingapp.models.LoginResponse;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
-    EditText etMobile,etpass;
+    EditText etMobile, etpass;
+    Button loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        loginButton = findViewById(R.id.btnlogin);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = "9874563210";
+                String password = "123";
+                String grant_type = "password";
 
+                Call<LoginResponse> call = RetrofitClient.getInstance().getApi().loginUser(username, password, grant_type);
+                call.enqueue(new Callback<LoginResponse>() {
+                    @Override
+                    public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) {
+                        // LoginResponse loginResponse = new LoginResponse();
+                        Log.i("loginResponse", response.body().getAccessToken());
+                        Toast.makeText(LoginActivity.this, "Access Token: " + response.body().getAccessToken(),
+                                Toast.LENGTH_LONG).show();
+
+//                        Log.i("Response access token", s + loginResponse.getAccessToken());
+//                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    }
+
+                    @Override
+                    public void onFailure(Call<LoginResponse> call, Throwable t) {
+                        Toast.makeText(LoginActivity.this, "Error " + t.getMessage(), Toast.LENGTH_LONG).show();
+                        Log.i("Error: ", t.getMessage());
+                    }
+                });
+
+            }
+        });
     }
 
     public void SignUp(View view) {
-        startActivity(new Intent(LoginActivity.this, SignupActivity.class));
+        //startActivity(new Intent(LoginActivity.this, SignupActivity.class));
     }
 }
---
+++ app/src/main/java/com/adverse/foodorderingapp/api/Api.java
@@ -0,0 +1,15 @@
+package com.adverse.foodorderingapp.api;
+import com.adverse.foodorderingapp.models.LoginResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface Api {
+    @FormUrlEncoded
+    @POST("token")
+    Call<LoginResponse> loginUser(@Field("username") String username,
+                                 @Field("password") String password,
+                                 @Field("grant_type") String grant_type);
+}
